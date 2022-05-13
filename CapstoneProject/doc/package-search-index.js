3115f87a6235628b568b219af451a1c4f64c0f78
Mira Haldar <mhaldar640@student.fuhsd.org>
2022-05-13 10:28:59 -0700

    new javadocs + subfolder

--- CapstoneProject/doc/package-search-index.js
+++ CapstoneProject/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mhaldar.shapes"},{"l":"screens"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"core"}];updateSearchResults();
